5e03af6dd1248fe9f0c48f5974e014095781f8e3
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2023-06-27 10:45:17 +0200

    Update New-Package.ps1

--- Build/New-Package.ps1
+++ Build/New-Package.ps1
@@ -1,1 +1,20 @@
+#requires -Modules BuildHelpers
 
+param(
+    [Parameter()]
+    [string]$BuildOutput_Path = (Join-Path -Path (Resolve-Path -Path "$PSScriptRoot/..") -ChildPath 'Build_Output')
+)
+
+# Variables
+$Repo_Path = Resolve-Path -Path "$PSScriptRoot/.."
+
+# Prepare build output
+if (Test-Path -Path $BuildOutput_Path) {
+    Write-Verbose 'Removing existing build output directory...'
+
+    Remove-Item -Path $BuildOutput_Path -Recurse -ErrorAction SilentlyContinue
+}
+
+Write-Verbose 'Creating build output directory...'
+
+New-Item -Path $BuildOutput_Path -Type Directory | Out-Null
